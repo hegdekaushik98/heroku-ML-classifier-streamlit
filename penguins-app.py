8d485c35fb5f178ff6a645c7e054ac7e4a6fc5f9
Kaushik Hegde <83506968+hegdekaushik98@users.noreply.github.com>
2021-06-22 23:01:48 +0530

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -15,7 +15,7 @@ This app predicts the **Palmer Penguin** species!
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+[Example CSV input file](https://raw.githubusercontent.com/hegdekaushik98/heroku-ML-classifier-streamlit/main/penguins_example.csv)
 """)
 
 # Collects user input features into dataframe
